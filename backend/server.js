5cf43566488cf293623e038a6d7677988373ed22
Aniket Sapra <aniketsapra2000@gmail.com>
2025-03-12 02:00:57 +0530

    added DBatlas

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/auth-app', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected')).catch(err => console.error(err));
